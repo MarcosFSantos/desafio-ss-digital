33121fe8a3b5902c4b3192da0491c9eb93f92025
MarcosFSantos <marcos.franca1673@gmail.com>
2025-02-13 15:12:47 -0300

    feat: Adicionar index.php para validar se o usuario ja possui um token nos cookies de sessões no modelo 2.

--- modelo2/public/index.php
+++ modelo2/public/index.php
@@ -1,3 +1,31 @@
 <?php
+require_once("database_connection.php");
 
-phpinfo();
+session_start(); # Permite a criação de sessões.
+# Vai para a página principal caso o token seja válido.
+if (isset($_COOKIE['token'])) {
+    $token = $_COOKIE['token'];
+    if (isValidToken(token: $token)) {
+        header(header: "Location: main.php");
+        exit();
+    }
+} elseif (isset($_SESSION['token'])) {
+    $token = $_SESSION['token'];
+    if (isValidToken(token: $token)) {
+        header(header: "Location: main.php");
+        exit();
+    }
+}
+
+# Vai para a tela de login caso o cookie com o token seja inexistente ou tenha um token inválido.
+header(header: "Location: login_email.php");
+exit();
+
+# Retorna o id do token do banco que conrresponder ao passado.
+function isValidToken(string $token): bool {
+    if (!$token) return false; # Se o token for nulo ou vazio, já retorna falso.
+
+    $database_token = execute_query(query: "SELECT Id FROM Tokens WHERE Token = ?", params: [$token]);
+
+    return !empty($database_token); # Retorna `true` se encontrou o token no banco, senão `false`.
+}
