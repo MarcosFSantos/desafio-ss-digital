443da61df6e283b474de20805d186ce38690e243
MarcosFSantos <marcos.franca1673@gmail.com>
2025-02-14 19:59:33 -0300

    fix: Ajustar Main para que receba as informacoes do token

--- modelo1/react-app/src/Main.js
+++ modelo1/react-app/src/Main.js
@@ -1,12 +1,24 @@
 import useAuth from "./hooks/useAuth";
 
 const Main = () => {
+  const { tokenInfo, loading } = useAuth();
+
+  // Exibe "Carregando..." enquanto verifica o token
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div>
       <div class="text-center">
         <main role="main" class="inner cover">
-          <h1 class="cover-heading">Bem vindo, {useAuth?.name}</h1>
-          <p class="lead">Você está conectado desde {useAuth?.lastLogin}</p>
+          <h1 class="cover-heading">
+            Bem vindo, {tokenInfo?.name || "Usuário"}
+          </h1>
+          <p class="lead">
+            Você está conectado desde{" "}
+            {tokenInfo?.login_time || "00:00:00 00/00/0000"}
+          </p>
           <p class="lead">
             <a href="index.php" class="btn btn-lg btn-danger">
               Sair
