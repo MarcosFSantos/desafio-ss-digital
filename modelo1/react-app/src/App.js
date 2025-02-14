376ae08f1525bb906c5ca779a9fd014fdef3000c
MarcosFSantos <marcos.franca1673@gmail.com>
2025-02-14 13:53:07 -0300

    feat: Adicionar protetor de rota em rota "/".

--- modelo1/react-app/src/App.js
+++ modelo1/react-app/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-//import PrivateRoute from "./routes/PrivateRoute";
+import PrivateRoute from "./routes/PrivateRoute";
 import LoginEmail from "./LoginEmail";
 import LoginPassword from "./LoginPassword";
 import Main from "./Main";
@@ -12,6 +12,9 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginEmail />} />
           <Route path="/login-senha" element={<LoginPassword />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Main />} />
+          </Route>
           <Route path="*" element={<LoginEmail />} />
         </Routes>
       </Router>
