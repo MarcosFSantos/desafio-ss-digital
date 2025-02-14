0b46dbb300bf5a71ae57a16c85dc1ba858b2d385
MarcosFSantos <marcos.franca1673@gmail.com>
2025-02-14 11:58:27 -0300

    feat: Adicionar rotas em App no app

--- modelo1/react-app/src/App.js
+++ modelo1/react-app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+//import PrivateRoute from "./routes/PrivateRoute";
+import LoginEmail from "./LoginEmail";
+import LoginPassword from "./LoginPassword";
+import Main from "./Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginEmail />} />
+          <Route path="/login-senha" element={<LoginPassword />} />
+          <Route path="*" element={<LoginEmail />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
